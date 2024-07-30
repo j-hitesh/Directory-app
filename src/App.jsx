323f4e4ffc37f8e482a17542bc244bb23eaa08a8
Hitesh Jangid <h26545080@gmail.com>
2024-07-30 22:35:02 +0530

    change tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import AddNewPerson from './components/newperson/AddNewPerson';
 import Retrieve from './components/Retrieve/Retrieve';
